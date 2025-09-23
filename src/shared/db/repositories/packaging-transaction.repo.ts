62cc43ff49c3195116d1cd8f17654c64fb013509
dothanhnhutis <dothanhnhutis@gmail.com>
2025-09-24 06:46:53 +0700

    packaging transaction repo

--- src/shared/db/repositories/packaging-transaction.repo.ts
+++ src/shared/db/repositories/packaging-transaction.repo.ts
@@ -71,6 +71,73 @@ export default class PackagingTransactionRepo {
       values: [],
     };
     try {
+      await this.fastify.transaction(async (client) => {
+        const columns = [
+          "type",
+          "from_warehouse_id",
+          "note",
+          "transaction_date",
+          "status",
+        ];
+        const packagingTransactionValues = [
+          data.type,
+          data.from_warehouse_id,
+          data.note,
+          data.transaction_date,
+          data.status,
+        ];
+        const placeholders = [
+          "$1::text",
+          "$2::text",
+          "$3::text",
+          "$4:timestampz",
+        ];
+
+        if (data.type == "TRANSFER") {
+          columns.push("to_warehouse_id");
+          values.push(data.to_warehouse_id);
+          placeholders.push("$5:text");
+        }
+
+        const { rows: new_packaging_transactions } =
+          await client.query<PackagingTransaction>({
+            text: `
+            INSERT INTO packaging_transactions (${columns.join(", ")}) 
+            VALUES (${placeholders.join(", ")})
+            RETURNING *;
+          `,
+            values: packagingTransactionValues,
+          });
+
+        const packagingTransactionItemValues: any[] = [
+          new_packaging_transactions[0].id,
+        ];
+
+        data.items.map((i, idx) => {
+          const index = idx * 3;
+          const signed_quantity =
+            data.type == "IMPORT" || data.type == "ADJUST"
+              ? i.quantity
+              : -i.quantity;
+
+          packagingTransactionItemValues.push(
+            i.packaging_id,
+            data.from_warehouse_id,
+            i.quantity,
+            signed_quantity
+          );
+          return `($1, $${index + 2}, $${index + 3}, $${index + 4})`;
+        });
+
+        await client.query({
+          text: `
+            INSERT INTO packaging_transaction_items (packaging_transaction_items, packaging_id, warehouse_id, quantity, signed_quantity) 
+            VALUES (${placeholders.join(", ")})
+            RETURNING *;
+          `,
+          values: packagingTransactionItemValues,
+        });
+      });
     } catch (error: unknown) {
       throw new CustomError({
         message: `PackagingTransactionRepo.createNewPackagingTransaction() method error: ${error}`,
--- src/types/packaging-transaction.d.ts
+++ src/types/packaging-transaction.d.ts
@@ -5,3 +5,15 @@ type PackagingInventory = {
   created_at: Date;
   updated_at: Date;
 };
+
+type PackagingTransaction = {
+  id: string;
+  type: string;
+  from_warehouse_id: string;
+  to_warehouse_id: string | null;
+  note: string;
+  transaction_date: Date;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
+};
